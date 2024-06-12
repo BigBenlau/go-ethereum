43c3dcf042daacca7a4c6ee6ccc50de6674c1052
Ben Lao <benlau413@gmail.com>
2024-06-12 17:39:59 +0800

    add print log in parallel update

--- parallel/global_var.go
+++ parallel/global_var.go
@@ -6,6 +6,7 @@ var Total_op_count_and_time map[string]map[string]int64
 
 func Update_total_op_count_and_time(opcode string, run_time int64) {
 	_, op_ok := Total_op_count_and_time[opcode]
+	fmt.Println("opcode: ", opcode, "op_ok: ", op_ok)
 	if !op_ok {
 		Total_op_count_and_time[opcode]["count"] = 1
 		Total_op_count_and_time[opcode]["total_time"] = run_time
