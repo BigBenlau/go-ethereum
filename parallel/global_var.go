f71a0a345f33614be742c7ae45226d495c7ebbdf
Ben Lao <benlau413@gmail.com>
2024-06-12 17:36:30 +0800

    remove parallel print log, update struct

--- parallel/global_var.go
+++ parallel/global_var.go
@@ -6,12 +6,9 @@ var Total_op_count_and_time map[string]map[string]int64
 
 func Update_total_op_count_and_time(opcode string, run_time int64) {
 	_, op_ok := Total_op_count_and_time[opcode]
-	fmt.Println(op_ok)
 	if !op_ok {
-		Total_op_count_and_time[opcode] = map[string]int64{
-			"count":      1,
-			"total_time": run_time,
-		}
+		Total_op_count_and_time[opcode]["count"] = 1
+		Total_op_count_and_time[opcode]["total_time"] = run_time
 	} else {
 		Total_op_count_and_time[opcode]["count"] += 1
 		Total_op_count_and_time[opcode]["total_time"] += run_time
