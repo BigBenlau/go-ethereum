3dfc25b32a01a1a5a4164c500b45bd2a7e86114f
Ben Lao <benlau413@gmail.com>
2024-06-12 17:46:56 +0800

    update global_var

--- parallel/global_var.go
+++ parallel/global_var.go
@@ -2,24 +2,24 @@ package parallel
 
 import "fmt"
 
-var Total_op_count_and_time map[string]map[string]int64
+var Total_op_count_map map[string]int64
+
+var Total_op_time_map map[string]int64
 
 func Update_total_op_count_and_time(opcode string, run_time int64) {
-	_, op_ok := Total_op_count_and_time[opcode]
+	_, op_ok := Total_op_count_map[opcode]
 	fmt.Println("opcode: ", opcode, "op_ok: ", op_ok)
 	if !op_ok {
-		Total_op_count_and_time[opcode]["count"] = 1
-		Total_op_count_and_time[opcode]["total_time"] = run_time
-	} else {
-		Total_op_count_and_time[opcode]["count"] += 1
-		Total_op_count_and_time[opcode]["total_time"] += run_time
+		Total_op_count_map[opcode] = 0
+		Total_op_time_map[opcode] = 0
 	}
+	Total_op_count_map[opcode] += 1
+	Total_op_time_map[opcode] += run_time
 }
 
 func Print_total_op_count_and_time() {
-	for op_code, time_value_list := range Total_op_count_and_time {
-		// op_count := time_value_list["count"]
-		op_run_time := time_value_list["total_time"]
+	for op_code, _ := range Total_op_count_map {
+		op_run_time := Total_op_time_map[op_code]
 		fmt.Println("Opcode name is", op_code, "Run time as nanos: ", op_run_time)
 	}
 }
