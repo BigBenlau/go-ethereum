a97af232a539857a0f1db9d64d5914ebaa7f48a5
Ben Lao <benlau413@gmail.com>
2024-06-13 16:10:51 +0800

    add extra log before error return

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -187,6 +187,9 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		// Get the operation from the jump table and validate the stack to ensure there are
 		// enough stack items available to perform the operation.
 		op = contract.GetOp(pc)
+
+		fmt.Println("pc is ", pc, " .Opcode name is", op.String(), "Before Error!!")
+
 		operation := in.table[op]
 		cost = operation.constantGas // For tracing
 		// Validate stack
